7f088d8d5763de7300481e34b16a5fb1d1b7fc42
Daniil Bondar <daniil.bondar49@gmail.com>
2022-07-28 20:16:49 +0300

    Tuples

---
+++ 003_complex_data_types_and_methods/sets.py
@@ -0,0 +1,7 @@
+# empty_set = {1, 1, 2, 3}     # Множество не допускает дубликаты
+# print(empty_set)
+
+
+courses = {'History', 'Math', 'Literature', 'Physics', 'Programming'}
+courses.add('English')
+print(courses)
