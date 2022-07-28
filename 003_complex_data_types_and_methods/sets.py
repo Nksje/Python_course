e4e406c3fcc1a5a86c728570f52ede0a97d73013
Daniil Bondar <daniil.bondar49@gmail.com>
2022-07-28 20:27:36 +0300

    Sets

--- 003_complex_data_types_and_methods/sets.py
+++ 003_complex_data_types_and_methods/sets.py
@@ -2,6 +2,16 @@
 # print(empty_set)
 
 
-courses = {'History', 'Math', 'Literature', 'Physics', 'Programming'}
-courses.add('English')
-print(courses)
+courses = ['History', 'Math', 'Literature', 'Physics', 'Programming', 'Math', 'History']
+set1 = {'Math', 'Physics', 'Programming', 'English'}
+set2 = {'English', 'History', 'Math', 'Art'}
+# courses.add('English')
+# print(courses)
+# print(courses.pop())     # Выкидывает случайные элемент
+# courses.clear()
+
+# print(set1.intersection(set2))    # Находит совпадения в двух сетах
+
+# print(set2.difference(set1))      # Находит чем отличается один от другого
+# print(set2.union(set1))           # Объединяет
+print(list(set(courses)))
