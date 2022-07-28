855c31c7fc3a46bb112cc8ad5727f581a74099a5
Daniil Bondar <daniil.bondar49@gmail.com>
2022-07-28 19:09:42 +0300

    datatypes

--- 003_complex_data_types_and_methods/main.py
+++ 003_complex_data_types_and_methods/main.py
@@ -1,7 +1,14 @@
 # empty_list = list()
 # print(type(empty_list))
 
-world = 'Hello world'
-filled_list = [123,[1, 2, 3,['hello', 5, 6], 7], 0.2341, True, None, 'Test', world]
-print(len(filled_list))
-print(filled_list[1][3][0])
+# world = 'Hello world'
+# filled_list = [123,[1, 2, 3,['hello', 5, 6], 7], 0.2341, True, None, 'Test', world]
+# print(len(filled_list))
+# print(filled_list[1][3][0])
+courses = ['History', 'Math', 'Literature', 'Physics', 'Programming']
+numbers = [1, 5, 6, 8, 3, 4, 2]
+
+# courses[0] = 'Art'
+# print(courses)
+courses.append('Art')
+print(courses)
