918fe5fbf6fe77a0f444e36e83540dc2e8e7ce59
Daniil Bondar <daniil.bondar49@gmail.com>
2022-07-28 20:16:42 +0300

    Tuples

---
+++ 003_complex_data_types_and_methods/tuples.py
@@ -0,0 +1,8 @@
+# empty_tuple = (1,)            # Кортеж в отличии от списка изменить нельзя
+# print(type(empty_tuple))
+
+# courses = ('History', 'Math', 'Literature', 'Physics', 'Programming')
+# print(courses.count('History'))
+
+
+
