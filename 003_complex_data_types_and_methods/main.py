e7850793b08ad9e941ffbc2768f6de7f62769a03
Daniil Bondar <daniil.bondar49@gmail.com>
2022-07-28 19:01:58 +0300

    datatypes

--- 003_complex_data_types_and_methods/main.py
+++ 003_complex_data_types_and_methods/main.py
@@ -2,5 +2,6 @@
 # print(type(empty_list))
 
 world = 'Hello world'
-filled_list = [123, 0.2341, True, None, 'Test', world]
+filled_list = [123,[1, 2, 3,['hello', 5, 6], 7], 0.2341, True, None, 'Test', world]
 print(len(filled_list))
+print(filled_list[1][3][0])
