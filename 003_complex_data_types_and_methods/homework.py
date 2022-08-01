cd06cc85c6493ba586d2dcb217aac82f44ac54bf
Daniil Bondar <daniil.bondar49@gmail.com>
2022-08-01 18:08:44 +0300

    Done

--- 003_complex_data_types_and_methods/homework.py
+++ 003_complex_data_types_and_methods/homework.py
@@ -1,7 +1,7 @@
 for num in range(1, 101):
-    if num % 3 == 0:
-        print(num, 'FIZZ')
-    if num % 5 == 0:
-        print(num, 'BUZZ')
     if num % 15 == 0:
         print(num, 'FIZZ BUZZ')
+    elif num % 5 == 0:
+        print(num, 'BUZZ')
+    elif num % 3 == 0:
+        print(num, 'FIZZ')
