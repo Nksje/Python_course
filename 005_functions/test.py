113ada5c787dcbb0b867f9b016c80e5a6476ba9b
Daniil Bondar <daniil.bondar49@gmail.com>
2022-08-02 20:19:51 +0300

    Test 5/7

---
+++ 005_functions/test.py
@@ -0,0 +1,47 @@
+from math import *
+# Task 1
+
+# name = input('Enter your name: ')
+# lastname = input('Enter your lastname: ')
+# age = input('Enter your age: ')
+# print(f'Hello, {name} {lastname}. Your age is: {age}')
+
+# Task 2
+
+# a = 5
+# b = 7
+# c = sqrt(a * a + b * b)
+# print(c)
+
+# Task 3
+
+# a, b, c = float(input('Enter A side: ')), float(input('Enter B side: ')),\
+#           float(input('Enter C side: '))
+# if c == a + b:
+#     print('Треугольный прямоугольный')
+# else:
+#     print('Треугольник непрямоугольный')
+
+
+# Task 4
+
+# list1 = list(input('Enter a list: '))
+# list1.reverse()
+# print(list1)
+
+# Task 5
+
+# a = (1, 2, 3, 4, 5, 8)
+# b = (8, 2, 5)
+#
+# listA = list(a)
+# listB = list(b)
+#
+# listA.insert(2, listB[0])
+# listA.insert(3, listB[1])
+# listA.insert(4, listB[2])
+# listA = tuple(listA)
+# print(listA)
+
+
+# Task 6
