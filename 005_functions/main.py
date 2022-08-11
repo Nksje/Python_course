297c8eef7af95f16f38c4cbdc09f25b82275ef48
Daniil <daniil.bondar49@gmail.com>
2022-08-11 22:26:07 +0300

    Text files

--- 005_functions/main.py
+++ 005_functions/main.py
@@ -1,20 +1,20 @@
 # def sum_two_numbers():
 #     print(x + y)
-#
-#
+
+
 # x = 10
 # y = 20
-#
+
 # sum_two_numbers()
-#
+
 # print(x)
 
 
 # def no_parameters():
 #     x = 25 ** 2
 #     return x  # После return действие заканчивается
-#
-#
+
+
 # no_parameters()
 
 
@@ -48,44 +48,44 @@
 #
 # tester(10, 32, 100, 5, 5, 77, 7675, 677, name='Daniil', lastname='Bondar')
 
-def perimeter(width, height):
-    return (width + height) * 2
-
-
-def area(width, height):
-    return width * height
-
-
-def count_materials(order):
-    result = {}
-    for key in order:
-        result[key] = [perimeter(order[key][0], order[key][1]) * order[key][2],
-                       area(order[key][0], order[key][1]) * order[key][2]]
-
-    return result
-
-
-def count_total_materials(materials_dict):
-    total_p = 0
-    total_a = 0
-    for key in materials_dict:
-        total_p += materials_dict[key][0] / 100
-        total_a += materials_dict[key][1] / 1000
-    return [total_p, total_a]
-
-
-carpets = {'small': [60, 60, 15], 'medium': [60, 90, 47], 'large': [90, 90, 20],
-           'xlarge': [125, 125, 5]}
-
-
-def main():
-    user_choice = input('1.Material by type\n2.Total material\n-->')
-    if user_choice == '1':
-        print(count_materials(carpets))
-    elif user_choice == '2':
-        print(count_total_materials(count_materials(carpets)))
-    else:
-        quit()
-
-
-main()
+# def perimeter(width, height):
+#     return (width + height) * 2
+#
+#
+# def area(width, height):
+#     return width * height
+#
+#
+# def count_materials(order):
+#     result = {}
+#     for key in order:
+#         result[key] = [perimeter(order[key][0], order[key][1]) * order[key][2],
+#                        area(order[key][0], order[key][1]) * order[key][2]]
+#
+#     return result
+#
+#
+# def count_total_materials(materials_dict):
+#     total_p = 0
+#     total_a = 0
+#     for key in materials_dict:
+#         total_p += materials_dict[key][0] / 100
+#         total_a += materials_dict[key][1] / 1000
+#     return [total_p, total_a]
+#
+#
+# carpets = {'small': [60, 60, 15], 'medium': [60, 90, 47], 'large': [90, 90, 20],
+#            'xlarge': [125, 125, 5]}
+#
+#
+# def main():
+#     user_choice = input('1.Material by type\n2.Total material\n-->')
+#     if user_choice == '1':
+#         print(count_materials(carpets))
+#     elif user_choice == '2':
+#         print(count_total_materials(count_materials(carpets)))
+#     else:
+#         quit()
+#
+#
+# main()
---
+++ 006_work_with_files/main.py
@@ -0,0 +1,3 @@
+with open('text_files//lorem.txt', 'r', encoding='UTF-8') as file:
+    print(len(file.read()))
+    
