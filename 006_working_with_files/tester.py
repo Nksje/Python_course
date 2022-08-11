309ea7b405612b9f8ae577268fcbdb2f0108ea7a
Daniil Bondar <daniil.bondar49@gmail.com>
2022-08-11 21:11:45 +0300

    Files

---
+++ 006_working_with_files/tester.py
@@ -0,0 +1,12 @@
+# 'r' - read
+# 'a' - append
+# 'w' - write
+# 'x' - create
+# 'r+' - write and read
+
+# with open('text_files/lorem.txt', 'r', encoding='UTF-8') as file:
+#     print(file.closed)
+#     print(file.name)
+    
+# print(file.closed)
+
