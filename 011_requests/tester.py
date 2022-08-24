44ba17cba59d43fdfb61b7fe02198fbc2c387089
Daniil Bondar <daniil.bondar49@gmail.com>
2022-08-25 02:06:56 +0300

    Homework BeautifulSoup

--- 011_requests/tester.py
+++ 011_requests/tester.py
@@ -2,16 +2,17 @@ import requests, lxml
 from bs4 import BeautifulSoup
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 "
+                  "Safari/537.36"}
+
 
 def get_btc_eur_value():
-    url = 'https://www.google.com/search?q=btc+to+eur&oq=btc+to+&aqs=chrome.0.0i512l2j69i57j0i512l7.2369j0j7&sourceid=chrome&ie=UTF-8'
+    url = 'https://www.google.com/search?q=btc+to+eur&oq=btc+to+&aqs=chrome.0.0i512l2j69i57j0i512l7.2369j0j7&sourceid' \
+          '=chrome&ie=UTF-8 '
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.content, 'lxml')
     result = soup.find('span', class_='pclqee')
     return float(result.text.encode('utf8').replace(b'\xc2\xa0', b'').replace(b',', b'.').decode('utf-8'))
-    
-
 
 
 def main():
@@ -31,5 +32,6 @@ def main():
             print(user_amount * get_btc_eur_value())
         else:
             print('Choice out of range!')
+
 
 main()
